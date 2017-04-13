4b4d8b413a3b8adf0edcf898da1ec41b8f29065f
Osman Hafızoğlu <osmanhafizogluu@gmail.com>
2017-04-13 03:28:54 +0300

    Beginner

--- BirKelimeBirİşlem/Form1.Designer.cs
+++ BirKelimeBirİşlem/Form1.Designer.cs
@@ -28,12 +28,329 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.bn9 = new System.Windows.Forms.Button();
+            this.bn8 = new System.Windows.Forms.Button();
+            this.bn7 = new System.Windows.Forms.Button();
+            this.bn6 = new System.Windows.Forms.Button();
+            this.bn5 = new System.Windows.Forms.Button();
+            this.bn4 = new System.Windows.Forms.Button();
+            this.bn3 = new System.Windows.Forms.Button();
+            this.bn2 = new System.Windows.Forms.Button();
+            this.bn1 = new System.Windows.Forms.Button();
+            this.btn_harf = new System.Windows.Forms.Button();
+            this.btn_sayi = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.groupBox1.Controls.Add(this.btn_harf);
+            this.groupBox1.Controls.Add(this.b9);
+            this.groupBox1.Controls.Add(this.b8);
+            this.groupBox1.Controls.Add(this.b7);
+            this.groupBox1.Controls.Add(this.b6);
+            this.groupBox1.Controls.Add(this.b5);
+            this.groupBox1.Controls.Add(this.b4);
+            this.groupBox1.Controls.Add(this.b3);
+            this.groupBox1.Controls.Add(this.b2);
+            this.groupBox1.Controls.Add(this.b1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(673, 141);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kelime";
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(17, 35);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(51, 33);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(83, 35);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(51, 33);
+            this.b2.TabIndex = 1;
+            this.b2.Text = " ";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(149, 35);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(51, 33);
+            this.b3.TabIndex = 2;
+            this.b3.Text = " ";
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(350, 35);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(51, 33);
+            this.b6.TabIndex = 5;
+            this.b6.Text = " ";
+            this.b6.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(284, 35);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(51, 33);
+            this.b5.TabIndex = 4;
+            this.b5.Text = " ";
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(218, 35);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(51, 33);
+            this.b4.TabIndex = 3;
+            this.b4.Text = " ";
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(552, 35);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(51, 33);
+            this.b9.TabIndex = 8;
+            this.b9.Text = " ";
+            this.b9.UseVisualStyleBackColor = true;
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(486, 35);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(51, 33);
+            this.b8.TabIndex = 7;
+            this.b8.Text = " ";
+            this.b8.UseVisualStyleBackColor = true;
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(420, 35);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(51, 33);
+            this.b7.TabIndex = 6;
+            this.b7.Text = " ";
+            this.b7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Aqua;
+            this.groupBox2.Controls.Add(this.btn_sayi);
+            this.groupBox2.Controls.Add(this.bn9);
+            this.groupBox2.Controls.Add(this.bn8);
+            this.groupBox2.Controls.Add(this.bn7);
+            this.groupBox2.Controls.Add(this.bn6);
+            this.groupBox2.Controls.Add(this.bn5);
+            this.groupBox2.Controls.Add(this.bn4);
+            this.groupBox2.Controls.Add(this.bn3);
+            this.groupBox2.Controls.Add(this.bn2);
+            this.groupBox2.Controls.Add(this.bn1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(13, 227);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(673, 126);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İşlem";
+            // 
+            // bn9
+            // 
+            this.bn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn9.Location = new System.Drawing.Point(552, 35);
+            this.bn9.Name = "bn9";
+            this.bn9.Size = new System.Drawing.Size(51, 33);
+            this.bn9.TabIndex = 8;
+            this.bn9.Text = " ";
+            this.bn9.UseVisualStyleBackColor = true;
+            // 
+            // bn8
+            // 
+            this.bn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn8.Location = new System.Drawing.Point(486, 35);
+            this.bn8.Name = "bn8";
+            this.bn8.Size = new System.Drawing.Size(51, 33);
+            this.bn8.TabIndex = 7;
+            this.bn8.Text = " =";
+            this.bn8.UseVisualStyleBackColor = true;
+            // 
+            // bn7
+            // 
+            this.bn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn7.Location = new System.Drawing.Point(420, 35);
+            this.bn7.Name = "bn7";
+            this.bn7.Size = new System.Drawing.Size(51, 33);
+            this.bn7.TabIndex = 6;
+            this.bn7.Text = " ";
+            this.bn7.UseVisualStyleBackColor = true;
+            // 
+            // bn6
+            // 
+            this.bn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn6.Location = new System.Drawing.Point(350, 35);
+            this.bn6.Name = "bn6";
+            this.bn6.Size = new System.Drawing.Size(51, 33);
+            this.bn6.TabIndex = 5;
+            this.bn6.Text = " ";
+            this.bn6.UseVisualStyleBackColor = true;
+            // 
+            // bn5
+            // 
+            this.bn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn5.Location = new System.Drawing.Point(284, 35);
+            this.bn5.Name = "bn5";
+            this.bn5.Size = new System.Drawing.Size(51, 33);
+            this.bn5.TabIndex = 4;
+            this.bn5.Text = " ";
+            this.bn5.UseVisualStyleBackColor = true;
+            // 
+            // bn4
+            // 
+            this.bn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn4.Location = new System.Drawing.Point(218, 35);
+            this.bn4.Name = "bn4";
+            this.bn4.Size = new System.Drawing.Size(51, 33);
+            this.bn4.TabIndex = 3;
+            this.bn4.Text = " ";
+            this.bn4.UseVisualStyleBackColor = true;
+            // 
+            // bn3
+            // 
+            this.bn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn3.Location = new System.Drawing.Point(149, 35);
+            this.bn3.Name = "bn3";
+            this.bn3.Size = new System.Drawing.Size(51, 33);
+            this.bn3.TabIndex = 2;
+            this.bn3.Text = " ";
+            this.bn3.UseVisualStyleBackColor = true;
+            // 
+            // bn2
+            // 
+            this.bn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn2.Location = new System.Drawing.Point(83, 35);
+            this.bn2.Name = "bn2";
+            this.bn2.Size = new System.Drawing.Size(51, 33);
+            this.bn2.TabIndex = 1;
+            this.bn2.Text = " ";
+            this.bn2.UseVisualStyleBackColor = true;
+            // 
+            // bn1
+            // 
+            this.bn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn1.Location = new System.Drawing.Point(17, 35);
+            this.bn1.Name = "bn1";
+            this.bn1.Size = new System.Drawing.Size(51, 33);
+            this.bn1.TabIndex = 0;
+            this.bn1.Text = " ";
+            this.bn1.UseVisualStyleBackColor = true;
+            // 
+            // btn_harf
+            // 
+            this.btn_harf.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_harf.Location = new System.Drawing.Point(260, 100);
+            this.btn_harf.Name = "btn_harf";
+            this.btn_harf.Size = new System.Drawing.Size(75, 23);
+            this.btn_harf.TabIndex = 9;
+            this.btn_harf.Text = "Harf Getir";
+            this.btn_harf.UseVisualStyleBackColor = true;
+            this.btn_harf.Click += new System.EventHandler(this.btn_harf_Click);
+            // 
+            // btn_sayi
+            // 
+            this.btn_sayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sayi.Location = new System.Drawing.Point(260, 83);
+            this.btn_sayi.Name = "btn_sayi";
+            this.btn_sayi.Size = new System.Drawing.Size(75, 23);
+            this.btn_sayi.TabIndex = 10;
+            this.btn_sayi.Text = "Sayı Getir";
+            this.btn_sayi.UseVisualStyleBackColor = true;
+            this.btn_sayi.Click += new System.EventHandler(this.btn_sayi_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 38);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Yeni Oyun";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 380);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_harf;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_sayi;
+        private System.Windows.Forms.Button bn9;
+        private System.Windows.Forms.Button bn8;
+        private System.Windows.Forms.Button bn7;
+        private System.Windows.Forms.Button bn6;
+        private System.Windows.Forms.Button bn5;
+        private System.Windows.Forms.Button bn4;
+        private System.Windows.Forms.Button bn3;
+        private System.Windows.Forms.Button bn2;
+        private System.Windows.Forms.Button bn1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- BirKelimeBirİşlem/Form1.cs
+++ BirKelimeBirİşlem/Form1.cs
@@ -16,5 +16,112 @@ namespace BirKelimeBirİşlem
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control item in groupBox1.Controls)
+            {
+                if (item is Button)
+                {
+                    Button btn = (Button)item;
+                    btn.Visible = false;
+                }
+            }
+            foreach (Control item in groupBox2.Controls)
+            {
+                if (item is Button)
+                {
+                    Button btn = (Button)item;
+                    btn.Visible = false;
+                }
+            }
+            btn_harf.Visible = true;
+            btn_sayi.Visible = true;
+            counter = 0;
+            counterh = 0;
+        }
+        int counter = 0;
+        int counterh = 0;
+        string[] harfler = { "A", "A", "B", "C", "Ç", "D", "E", "E", "F", "G", "Ğ", "H", "I", "İ", "İ", "J", "K", "L", "M", "N", "O", "P", "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z" };
+            Random rnd = new Random();
+        
+        private void btn_harf_Click(object sender, EventArgs e)
+        {
+            int a = rnd.Next(0, harfler.Length);
+            counter++;
+            if (counter == 1)
+            { b1.Visible = true; b1.Text = harfler[a]; }
+            else if (counter == 2)
+            {
+                b2.Visible = true; b2.Text = harfler[a];
+            }
+            else if (counter == 3)
+            {
+                b3.Visible = true; b3.Text = harfler[a];
+            }
+            else if (counter == 4)
+            {
+                b4.Visible = true; b4.Text = harfler[a];
+            }
+            else if (counter == 5)
+            {
+                b5.Visible = true; b5.Text = harfler[a];
+            }
+            else if (counter == 6)
+            {
+                b6.Visible = true; b6.Text = harfler[a];
+            }
+            else if (counter == 7)
+            {
+                b7.Visible = true; b7.Text = harfler[a];
+            }
+            else if (counter == 8)
+            {
+                b8.Visible = true; b8.Text = harfler[a];
+                b9.Visible = true; b9.Text = "?";
+            }
+        }
+
+        private void btn_sayi_Click(object sender, EventArgs e)
+        {
+            int a = rnd.Next(1,11);
+            counterh++;
+            if (counterh == 1)
+            { bn1.Visible = true; bn1.Text = a.ToString(); }
+            else if (counterh == 2)
+            {
+                bn2.Visible = true; bn2.Text = a.ToString();
+            }
+            else if (counterh == 3)
+            {
+                bn3.Visible = true; bn3.Text = a.ToString();
+            }
+            else if (counterh == 4)
+            {
+                bn4.Visible = true; bn4.Text = a.ToString();
+            }
+            else if (counterh == 5)
+            {
+                bn5.Visible = true; bn5.Text = a.ToString();
+            }
+            else if (counterh == 6)
+            {
+                bn6.Visible = true; bn6.Text = a.ToString();
+            }
+            else if (counterh == 7)
+            {
+                bn7.Visible = true; bn7.Text = rnd.Next(25,35).ToString();
+            }
+            else if (counterh == 8)
+            {
+                bn8.Visible = true;
+                bn9.Visible = true; bn9.Text = rnd.Next(350,450).ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1_Load(new object { }, new EventArgs { });
+        }
     }
 }
